84bc91d9627b4709d91b3ecb543c54b47f35c9f3
monikasaipitta <88325315+monikasaipitta@users.noreply.github.com>
2022-01-06 19:39:29 +0530

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,23 @@
+class Assignment1{
+     
+        public static void main(String[] args)
+        {
+            int num1 = 20, num2 = 10, sum = 0, sub=0, mult=0, div=0;
+    
+            System.out.println("num1 = " + num1);
+            System.out.println("num2 = " + num2);
+            
+            sum = num1 + num2;
+            System.out.println("The sum = " + sum);
+    
+           sub = num1 - num2;
+            System.out.println("The sub = " + sub);
+    
+            mult = num1 * num2;
+            System.out.println("The mult = " + mult);
+    
+            div = num1 / num2;
+            System.out.println("The div = " + div);
+        }
+    
+    }
